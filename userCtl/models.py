2a59690cc079c2e732ae5d099a161ce63c7f2120
dccrazyboy <dccrazyboy@gmail.com>
2013-03-03 16:13:49 +0800

    userCtl finish

--- userCtl/models.py
+++ userCtl/models.py
@@ -4,8 +4,11 @@ from django.db import models
 from mongoengine import *
 
 class User(Document):
-    userName = StringField(max_length=50,required=True,unique=True)
-    userPw = StringField(max_length=50,required=True)
+    '''
+    store all user info
+    '''
+    usr_name = StringField(max_length=50,required=True,unique=True)
+    usr_pwd = StringField(max_length=50,required=True)
 
 def delUserDB():
     '''
@@ -16,27 +19,77 @@ def delUserDB():
     '''
     User.drop_collection()
 
-def regUser(name,password):
+def regist_usr_account(usr_name, usr_pwd):
     '''
     reg user
-    return True/False
+    @return True/False
     '''
     try:
-        User(userName=name,userPw=password).save()
+        User(usr_name=usr_name, usr_pwd=usr_pwd).save()
         return True
     except:
         return False
 
-def delUser(name):
+def check_usr_pwd(usr_name, usr_pwd):
+    '''
+    check pw
+    @return True/False
+    '''
+    try:
+        userlist = User.objects(usr_name=usr_name)
+        user = userlist[0]
+        if user.usr_pwd == usr_pwd:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def check_usr_exist(usr_name):
+    '''
+    check user exist
+    @return True/False
+    '''
+    if User.objects(usr_name=usr_name):
+        return True
+    else:
+        return False
+
+def get_all_usr_info():
+    '''
+    get all user info
+    
+    @return list
+        each item has name/pwd
+    
+    @example
+        all_user = get_all_usr_info()
+        for u in all_user:
+            print u.usr_name,u.usr_pwd
+    '''
+    return User.objects()
+
+def search_usr_info(usr_name):
+    '''
+    search user info
+
+    @return userinfo/None
+    
+    @example
+        u = search_usr_info('name')
+        if u:
+            print u.usr_name,u.usr_pwd
+    '''
+    try:
+        u = User.objects(usr_name=usr_name)[0]
+        return u
+    except:
+        return None
+
+
+def del_user(usr_name):
     '''
     del the user
     '''
-    User.objects(userName=name).delete()
+    User.objects(usr_name=usr_name).delete()
     return True
-
-def addFriend(name1,name2):
-    pass
-
-
-
-
--- userCtl/tests.py
+++ userCtl/tests.py
@@ -9,15 +9,44 @@ from django.test import TestCase
 from models import *
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-    def test_user(self):
+    def test_reg_user(self):
         delUserDB()
-        self.assertEqual(True,regUser('dc','123456'))
-        self.assertEqual(False,regUser('dc','123456'))
-        #self.assertEqual(True,delUser('dc'))
+        self.assertTrue(regist_usr_account('dc','123456'))
+        self.assertFalse(regist_usr_account('dc','123456'))
+        
+    def test_del_user(self):
+        delUserDB()
+        self.assertTrue(regist_usr_account('dc','123456'))
+        self.assertTrue(del_user('dc'))
 
+    def test_exist_user(self):
+        delUserDB()
+        self.assertTrue(regist_usr_account('dc','123456'))
+        self.assertTrue(check_usr_exist('dc'))
+        self.assertTrue(del_user('dc'))
+        self.assertFalse(check_usr_exist('dc'))
 
+    def test_check_pwd(self):
+        delUserDB()
+        self.assertTrue(regist_usr_account('dc','123456'))
+        self.assertTrue(check_usr_pwd('dc','123456'))
+        self.assertTrue(del_user('dc'))
+        self.assertFalse(check_usr_pwd('dc','654321'))
+
+    def test_get_all_user(self):
+        delUserDB()
+        self.assertTrue(regist_usr_account('dc1','123456'))
+        self.assertTrue(regist_usr_account('dc2','123456'))
+        self.assertTrue(regist_usr_account('dc3','123456'))
+        all_user = get_all_usr_info()
+        all_name = [u.usr_name for u in all_user]
+        self.assertEqual(len(all_name),3)
+        self.assertIn('dc1',all_name)
+        self.assertIn('dc2',all_name)
+        self.assertIn('dc3',all_name)
+
+    def test_get_all_user(self):
+        delUserDB()
+        self.assertIsNone(search_usr_info('dc'))
+        self.assertTrue(regist_usr_account('dc','123456'))
+        self.assertIsNotNone(search_usr_info('dc'))
