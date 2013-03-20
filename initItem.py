ad11f220a3721c64eb7a86ff7b318103166d8436
fz1989 <fz1989fz@gmail.com>
2013-03-20 12:05:45 +0800

    add items

--- initItem.py
+++ initItem.py
@@ -3,9 +3,25 @@
 from django.core.management import setup_environ
 import os
 import sys
-
+from itemSettings import *
+item_dict = {u'科技': 1,
+             u'生活': 2,
+             u'医疗': 3,
+             u'文学': 4,
+             u'教育': 5,
+             u'旅行': 6,
+             u'新闻': 7,
+             u'娱乐': 8,
+             u'艺术': 9,
+             u'人物': 10}
 def add():
-
+    for cate_name in cate_list:
+        add_cate(cate_name)
+    for item in item_list:
+        print item[4]
+        add_item(item[0], item[1], item_dict[item[4]], item[2], item[3],item[4])
+    for prob in prob_list:
+        add_prob(prob)
 
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ReadOneServer.settings")
@@ -13,4 +29,4 @@ if __name__ == '__main__':
     from userCtl.models import *
     from itemCtl.models import *
     add()
-    print 'hello'
+    print 'Finish init item data'
